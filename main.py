5745d66bd6717814dc83c5d4f496780f2af44305
Tianmin Lyu <36526042+thecrossed@users.noreply.github.com>
2023-03-09 21:01:47 +0100

    test read json

--- main.py
+++ main.py
@@ -81,6 +81,14 @@ tianmin_players = {
 student_json = json.dumps(tianmin_players) 
 with open("student.json", "w") as outfile:
     json.dump(student_json, outfile)
+    
+    
+# load data from student json
+with open('sample.json') as user_file:
+    file_contents = user_file.read()
+    
+parsed_json = json.loads(json.loads(file_contents))
+print(parsed_json['tianminlyu'])
 
 def student_df(student_data):
     """
